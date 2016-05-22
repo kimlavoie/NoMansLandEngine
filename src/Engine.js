aaeb43e368705d40859e5d1a031df2f8f42e0387
Kim Lavoie <kim.lavoie.14@gmail.com>
2016-05-22 12:52:38 -0400

    Second commit, added PIXI to the mix

--- src/Engine.js
+++ src/Engine.js
@@ -3,14 +3,18 @@ var Scene = require("./Scene");
 function Engine(){
 	var that = this;
 	this.config = {};
+	this.renderer = 0;
+	this.currentScene = 0;
 	this.init = function(config){
 		that.config = config;
+		that.renderer = new PIXI.WebGLRenderer(800,600);	//maybe set dimensions in config...
+		document.body.appendChild(that.renderer.view);			//maybe set the element in config...
 	};
 	this.start = function(){
-		var scene = new Scene(that.config);
-		that.config.scenes[that.config.firstScene](scene);
+		that.currentScene = new Scene(that.config);
+		that.config.scenes[that.config.firstScene](that.currentScene);
 		//do something with the prepared scene
-		console.log(scene);
+		console.log(that.currentScene);
 	};
 }
 module.exports = Engine;
